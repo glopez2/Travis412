dc41bf38b673e0ec0370126563b469f432aeb4f7
gerardo <glopez2@students.towson.edu>
2021-02-21 16:32:02 -0500

    starting of multplication, division, and power of. should break build

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -17,4 +17,35 @@ public class SimpleCalculatorTest {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.subtract(5, 3), 2);
 	}
+	
+	@Test
+	public void testMultiply() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.multiply(5, 5), 25);
+	}
+	
+	@Test
+	public void testDivide() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.divide(5, 5), 1);
+	}
+	
+	@Test (expected = ArithmeticException.class)
+	public void testDivideByZero() {
+		try {
+			SimpleCalculator calc = new SimpleCalculator();
+			calc.divide(5,0);
+			fail("Expected an IllegalArgumentException to be thrown");
+		} catch (IllegalArgumentException e) {
+			assertEquals(e.getMessage(), "Division by zero is not supported");
+		} catch (Throwable t) {
+			fail("Expected an IllegalArgumentException to be thrown");
+		}
+	}
+	
+	@Test
+	public void testPower() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.power(2, 3), 8);
+	}
 }
